5b28b17d8ccd9083987a523571331d581f2fcffb
Linda Hrdinová <hrlin@seznam.cz>
2024-05-29 13:28:45 +0200

    Datluj.cz, fáze 1

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,12 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const Wordbox = ({ word }) => {
-  const [lettersLeft, setLettersLeft] = useState(word);  
-  
-  return (
-    <div className="wordbox">{lettersLeft}</div>
-  );
+  const [lettersLeft, setLettersLeft] = useState(word);
+
+  useEffect(() => {
+    const handleKeyUp = (event) => {
+      setLettersLeft((prevLettersLeft) => {
+        if (prevLettersLeft.length === 0) {
+          return prevLettersLeft;
+        }
+
+        const firstLetter = prevLettersLeft.charAt(0).toLowerCase();
+        if (event.key.toLowerCase() === firstLetter) {
+          return prevLettersLeft.slice(1);
+        }
+        return prevLettersLeft;
+      });
+    };
+
+    document.addEventListener('keyup', handleKeyUp);
+
+    // Funkce Cleanup pro odstranění posluchače událostí
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  return <div className="wordbox">{lettersLeft}</div>;
 };
+
+/*
+<>
+    {console.log(lettersLeft.length)}
+    {lettersLeft.length === 0 ? null : (
+    <div className="wordbox">{lettersLeft}</div>
+  )}
+</>
+*/
 
 export default Wordbox;
